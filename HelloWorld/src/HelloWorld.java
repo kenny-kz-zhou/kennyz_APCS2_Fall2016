af62817b83ac5f489342dfa2c25f3f9dbff909e1
kenny-kz-zhou <kennyzhou1@gmail.com>
2016-08-29 09:39:42 -0700

    Pikachu First

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+// Author: Kenny Zhou
+// Proj. Name: 
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
