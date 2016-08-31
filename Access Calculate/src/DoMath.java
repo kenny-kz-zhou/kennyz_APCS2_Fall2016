eeb1535a2d7383a359dbb09ac9ec720872b9cd5c
kenny-kz-zhou <kennyzhou1@gmail.com>
2016-08-31 10:49:11 -0700

    Calculate Library 8/31/16

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author APCS2
+ *
+ */
+public class DoMath {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculate.square(5);
+
+	}
+
+}
